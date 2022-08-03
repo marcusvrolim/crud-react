210b071422758b0143513344db34e0e37880fb63
marcusvrolim <marcusrolimcontato@gmail.com>
2022-08-03 16:52:38 -0400

    #001 - crud-react inicial

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css';
+import React from 'react';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props =>
+    <footer className="footer">
+        <spam className="me-1">Desenvolvido por Marcus Rolim com </spam><i className='fa fa-heart text-danger'> </i> 
+        <strong> Cod<span className='text-danger'>3</span>r</strong>
+    </footer>
---
+++ frontend/src/components/template/Logo.jsx
@@ -0,0 +1,14 @@
+import './Logo.css';
+import logo from '../../assets/imgs/logo.png';
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props =>
+    <aside className="logo">
+        <Link to="/" className="logo">
+            <img src={logo} alt="logo" />
+        </Link>
+    </aside>
+
+    
